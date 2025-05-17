5e966193799a138ad317c099f884a2e0f2cb34c8
Matias Alfaro Iglesias <138666313+mat1as75@users.noreply.github.com>
2025-05-17 17:15:56 -0300

    Update CommerceNewCommerce.java

--- TallerJava/src/main/java/org/tallerJava/commerceModule/interfase/event/out/CommerceNewCommerce.java
+++ TallerJava/src/main/java/org/tallerJava/commerceModule/interfase/event/out/CommerceNewCommerce.java
@@ -8,9 +8,6 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class CommerceNewCommerce {
-    //no uso VehiculoDTO para maneter bajo el acoplamiento
-    //de lo contrario el modulo de Peaje que es quien va a escuchar este evento
-    //va a tener una dependencia transitiva con el DTO
     private int rut;
     private String email;
     private String password;

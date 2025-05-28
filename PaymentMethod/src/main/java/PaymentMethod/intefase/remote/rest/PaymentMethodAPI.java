f29a33d3bd2f6bd71f1c85701beadcfb8f22cf5d
Ivan Dos Santos <102613357+AIVICODE@users.noreply.github.com>
2025-05-27 22:23:48 -0300

    Return de errores del endpoint Metodo de pago (#22)

--- PaymentMethod/src/main/java/PaymentMethod/intefase/remote/rest/PaymentMethodAPI.java
+++ PaymentMethod/src/main/java/PaymentMethod/intefase/remote/rest/PaymentMethodAPI.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
 @Path("/api")
@@ -21,7 +22,14 @@ public class PaymentMethodAPI {
 
     @POST
     @Transactional
-    public PaymentResult processPayment(PaymentDataDTO paymentData) { //long number, String brand, String expirationDate, double amount
-        return PaymentResult.OK;
+    public Response processPayment(PaymentDataDTO paymentData) { //long number, String brand, String expirationDate, double amount
+        if(paymentData.getAmount() > 2000){
+            return Response
+                    .status(Response.Status.PAYMENT_REQUIRED) // HTTP 402
+                    .entity("Fondos insuficientes.")
+                    .build();
+        }else{
+            return Response.ok(PaymentResult.OK).build();
+        }
     }
 }

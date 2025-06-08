4cf1f53fd7a6477f27504946797122cb665011ee
mat1as75 <matialfaro757575@gmail.com>
2025-06-07 21:01:07 -0300

    Add SoapClient

---
+++ TallerJava/src/main/java/org/tallerJava/transferModule/infrastructure/remote/soap/generated/ObjectFactory.java
@@ -0,0 +1,35 @@
+package org.tallerJava.transferModule.infrastructure.remote.soap.generated;
+
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the bancoclientemock package. 
+ * <p>An ObjectFactory allows you to programmatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ *
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: bancoclientemock
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link DepositResponse }
+     */
+    public DepositResponse createDepositResponse() {
+        return new DepositResponse();
+    }
+
+}
---
+++ TallerJava/src/main/java/org/tallerJava/transferModule/infrastructure/remote/soap/generated/package-info.java
@@ -0,0 +1,2 @@
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://soap.ws.interfaces.bancoclientemock/")
+package org.tallerJava.transferModule.infrastructure.remote.soap.generated;

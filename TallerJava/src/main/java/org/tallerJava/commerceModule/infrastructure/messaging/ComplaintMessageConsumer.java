d1b9f7b4e022b7ad8f8197dcdfe4bae9d39da807
Ivan <ivandosa33@gmail.com>
2025-06-26 21:07:05 -0300

    fix

--- TallerJava/src/main/java/org/tallerJava/commerceModule/infrastructure/messaging/ComplaintMessageConsumer.java
+++ TallerJava/src/main/java/org/tallerJava/commerceModule/infrastructure/messaging/ComplaintMessageConsumer.java
@@ -15,9 +15,8 @@ import org.tallerJava.commerceModule.domain.repo.CommerceRepository;
         @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:/jms/queue/ComplaintQueue"),
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "jakarta.jms.Queue"),
         @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
-        @ActivationConfigProperty
-                // "maxSession" Establece el número máximo de consumidores que estarán procesando
-                (propertyName = "maxSession", propertyValue = "1")
+        @ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1")
+                                    // "maxSession" Establece el número máximo de consumidores que estarán procesando
 })
 public class ComplaintMessageConsumer implements MessageListener {
     private static final Logger log = Logger.getLogger(ComplaintMessageConsumer.class);
@@ -30,14 +29,10 @@ public class ComplaintMessageConsumer implements MessageListener {
         try {
             if (message instanceof ObjectMessage objectMessage) {
                 ComplaintMessage complaintMessage = (ComplaintMessage) objectMessage.getObject();
-
                 // Acá deberia de ir la consulta al endpoint del LLM
                 String sentiment = "POSITIVO"; //LLM RESPONSE. Uppercase sensitive
 
                 commerceRepository.createComplaint(complaintMessage.getRutCommerce(), complaintMessage.getMessage(), sentiment);
-
-
-
             }
         } catch (JMSException e) {
             throw new RuntimeException("Error al procesar JMS", e);
